e80f5ad13bc0b699ded2cf408014e2cfd84d09f4
Chris Cassano <glitch0@gmail.com>
2015-10-14 14:59:20 -0700

    wifiname and password are passed

--- src/http.c
+++ src/http.c
@@ -331,7 +331,7 @@ http_nodogsplash_callback_action(request *r,
 	switch(action) {
 	case AUTH_MAKE_AUTHENTICATED:
 		// Set the wifi info
-		debug(LOG_DEBUG, "username: %s, password: %s", authtarget->username, authtarget->password)
+		debug(LOG_DEBUG, "username: %s, password: %s", authtarget->username, authtarget->password);
 		auth_client_action(ip,mac,action);
 		http_nodogsplash_redirect(r, redir);
 		break;
